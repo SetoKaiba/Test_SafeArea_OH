4112e000d52988935bb620b9f392173d805735f3
Seto <61304189@qq.com>
2024-05-26 14:57:41 +0800

    refactor

--- Assets/Scripts/OHSafeArea.cs
+++ Assets/Scripts/OHSafeArea.cs
@@ -2,27 +2,27 @@ using UnityEngine;
 
 public class OHSafeArea : MonoBehaviour
 {
-    private static OHSafeArea _ohSafeArea;
+
     public static Rect safeArea
     {
         get
         {
-            if (_ohSafeArea) return _ohSafeArea._safeArea;
-            var go = new GameObject("OHSafeArea");
-            DontDestroyOnLoad(go);
-            _ohSafeArea = go.AddComponent<OHSafeArea>();
-            _ohSafeArea._safeArea = Screen.safeArea;
-            return _ohSafeArea._safeArea;
+            if (_init) return _safeArea;
+            Init();
+            return _safeArea;
         }
     }
+    private static bool _init;
 
-    Rect _safeArea = Rect.zero;
+    private static Rect _safeArea = Rect.zero;
     
-    void Awake()
+    private static void Init()
     {
+        _safeArea = Screen.safeArea;
         var openHarmonyJsObject = new OpenHarmonyJSObject("SafeArea");
         var callback =
             new OpenHarmonyJSCallback(args => { _safeArea = Screen.safeArea; });
         openHarmonyJsObject.Call("onAvoidAreaChange", callback);
+        _init = true;
     }
 }
